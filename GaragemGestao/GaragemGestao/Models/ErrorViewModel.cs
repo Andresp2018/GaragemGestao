8e77e94e95b5b7cac59f696855a907c2a42ebb64
Andre Pires <Andre.Silva.Pires@formandos.cinel.pt>
2021-03-03 14:51:36 +0000

    Initial

---
+++ GaragemGestao/GaragemGestao/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GaragemGestao.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
